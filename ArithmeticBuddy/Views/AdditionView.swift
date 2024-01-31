cb34486fa1cba9c3b7ea060f1f7cb7802afecd6b
Veda Cunniffe <vcunniffe@lcs.on.ca>
2024-01-31 11:49:47 -0500

    Created the project and finished initial project organization.

---
+++ ArithmeticBuddy/Views/AdditionView.swift
@@ -0,0 +1,18 @@
+//
+//  AdditionView.swift
+//  ArithmeticBuddy
+//
+//  Created by Veda Niav Cunniffe on 2024-01-31.
+//
+
+import SwiftUI
+
+struct AdditionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AdditionView()
+}
