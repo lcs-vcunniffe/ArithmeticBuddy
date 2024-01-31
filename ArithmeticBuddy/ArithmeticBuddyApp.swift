cf692a9ea74109cb89a65a0f173b3749274feef5
Veda Cunniffe <vcunniffe@lcs.on.ca>
2024-01-31 11:42:45 -0500

    Initial Commit

---
+++ ArithmeticBuddy/ArithmeticBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArithmeticBuddyApp.swift
+//  ArithmeticBuddy
+//
+//  Created by Veda Niav Cunniffe on 2024-01-31.
+//
+
+import SwiftUI
+
+@main
+struct ArithmeticBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
